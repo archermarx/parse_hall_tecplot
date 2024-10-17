9818ca1d1ff27a1d8a8893c722355e773bb6bf0b
Thomas Marks <marksta@umich.edu>
2024-10-17 11:42:03 -0400

    add interpolation function

--- parse.py
+++ parse.py
@@ -1,24 +1,32 @@
 import numpy as np
 
-def strip_brackets(s, l, r = None):
-    if r is None:
-        r = l
-    inside = False
-    acc = ""
-    for c in s:
-        if inside:
-            if c == r:
-                break
-            acc += c;
-        else:
-            if c == l:
-                inside = True
-    return acc
 
-def parse(filename):
+def parse_tecplot(filename):
+    """
+    Parse a tecplot file from Hall2De, extracting nodal and cell-centered data
+    """
     # read file and skip first line
     f = open(filename, "r")
     f.readline()
+
+    def strip_brackets(s, l, r = None):
+        """
+        for a string of the form str = "this is a {string}!"
+        return "string" is strip_brackets(str, '{', '}')
+        """
+        if r is None:
+            r = l
+        inside = False
+        acc = ""
+        for c in s:
+            if inside:
+                if c == r:
+                    break
+                acc += c;
+            else:
+                if c == l:
+                    inside = True
+        return acc
 
     # read variables
     variables = []
@@ -94,6 +102,43 @@ def parse(filename):
 
     cell_data["z(m)"] = z_c
     cell_data["r(m)"] = r_c
+    
+    # reorder cell vars so geometry starts first
+    cell_vars = ["z(m)", "r(m)", "i0", "i1", "i2", "i3"]
+    cell_vars += variables[cell_lo-1:]
+    cell_data = {k: cell_data[k] for k in cell_vars}
 
     f.close()
     return nodal_data, cell_data
+
+def interp_to_cells(nodal_data, cell_data):
+    """
+    Interpolate all nodal variables to cell centers, insert into cell-centered data dictionary
+    """
+    inds = [cell_data[f"i{i}"] for i in range(4)]
+    
+    zc = cell_data["z(m)"]
+    rc = cell_data["r(m)"]
+    
+    # compute interpolation weights
+    dzs = [(nodal_data["z(m)"][i] - zc)**2 for i in inds]
+    drs = [(nodal_data["r(m)"][i] - rc)**2 for i in inds]
+    wts = [1 / (dz + dr) for (dz, dr) in zip (dzs, drs)]
+    sumwt = sum(wts)
+    wts = np.array([w / sumwt for w in wts])
+
+    # create new dict 
+    itp = {"z(m)": zc, "r(m)": rc}
+
+    # interpolate nodal vars to cell centers
+    for k in nodal_data.keys():
+        v0, v1, v2, v3 = [nodal_data[k][i] for i in inds]
+        itp[k] = wts[0]*v0 + wts[1]*v1 + wts[2]*v2 + wts[3]*v3 
+
+    # add cell-centered vars at end
+    for (i, k) in enumerate(cell_data.keys()):
+        # don't re-add z, r, or indices
+        if (i >= 6):
+            itp[k] = cell_data[k]
+
+    return itp, wts
